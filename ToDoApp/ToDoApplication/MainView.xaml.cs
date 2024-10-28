5a57c26bd596487e8d952d84e6ef0f3db2e2caf6
hostser <andrey19.add@gmail.com>
2024-10-28 17:29:07 +0300

    v0.7

--- ToDoApp/ToDoApplication/MainView.xaml.cs
+++ ToDoApp/ToDoApplication/MainView.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace ToDoApplication.Views
+namespace ToDoApplication
 {
     /// <summary>
     /// Логика взаимодействия для MainView.xaml
